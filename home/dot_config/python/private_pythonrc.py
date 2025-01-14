f0f356ebd69a972c0b20e940fe9afe818fa88bb2
Ian Jukes <ianjukes@users.noreply.github.com>
2025-01-14 17:12:58 +0000

    Update pythonrc.py to check for 3.13.0

--- home/dot_config/python/private_pythonrc.py
+++ home/dot_config/python/private_pythonrc.py
@@ -1,18 +1,41 @@
-import os
-import atexit
-import readline
+import sys
 
-history = os.path.join( os.environ['XDG_CACHE_HOME'], 'python_history' )
-try:
-  readline.read_history_file( history )
-except OSError:
-  pass
+if sys.version_info < (3, 13):
 
-def write_history():
+  import atexit
+  import os
+  import readline
+  import time
+  from pathlib import Path
+
+  def write_history(path):
+      import os
+      import readline
+      try:
+          readline.write_history_file(path)
+      except OSError:
+          pass
+
+  cache_home = os.environ.get("XDG_CACHE_HOME")
+  if cache_home is None:
+      cache_home = Path.home() / ".cache"
+  else:
+      cache_home = Path(cache_home)
+
+  history_path = cache_home / "python_history"
+  if history_path.is_dir():
+      raise OSError(f"'{history_path}' cannot be a directory")
+
+  history = str(history_path)
+
   try:
-    readline.write_history_file( history )
-  except OSError:
-    pass
+      readline.read_history_file(history)
+  except FileNotFoundError:
+      pass
 
-atexit.register( write_history )
+  # Prevents creation of default history if custom is empty
+  if readline.get_current_history_length() == 0:
+      readline.add_history(f'# File created at {time.asctime()}')
 
+  atexit.register(write_history, history)
+  del (atexit, os, readline, time, history, write_history)
